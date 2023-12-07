c3d111caa0e4fa42e2339972645a18fb089dd657
Loic Gouarin <loic.gouarin@gmail.com>
2023-12-07 17:19:54 +0100

    update

--- material/1.initialization/examples/circle.cpp
+++ material/1.initialization/examples/circle.cpp
@@ -9,18 +9,4 @@
 
 #include <splinart/build_img.hpp>
 #include <splinart/imshow.hpp>
-
-using namespace xt::placeholders;
-
-int main(int argc, char* argv[])
-{
-    std::size_t img_size   = 1000;
-    std::size_t nb_samples = 10000;
-
-    splinart::Circle circle({0.5, 0.5}, 0.3, 75);
-
-    auto img = splinart::build_img({img_size, img_size}, {circle}, nb_samples);
-    splinart::imshow(img);
-
-    return 0;
-}
+usingnamespacext::placeholders;intmain(intargc,char*argv[]){std::size_timg_size=1000;std::size_tnb_samples=10000;splinart::Circlecircle({0.5,0.5},0.3,75);autoimg=splinart::build_img({img_size,img_size},{circle},nb_samples);splinart::imshow(img);return0;}
